c757f813b924aa44d1b64dee9549b6ca5dd64524
Дарья <Nencytime.14.92@yandex.ru>
2022-09-20 20:18:26 +0300

    Исправляет ошибки

--- gulpfile.js
+++ gulpfile.js
@@ -72,12 +72,10 @@ const createWebp = () => {
 
 const svg = () =>
   gulp.src(['source/img/**/*.svg', '!source/img/sprite/*.svg'])
-  .pipe(svgo())
   .pipe(gulp.dest('build/img'));
 
 const sprite = () => {
   return gulp.src('source/img/sprite/*.svg')
-  .pipe(svgo())
   .pipe(svgstore({
   inlineSvg: true
 }))
